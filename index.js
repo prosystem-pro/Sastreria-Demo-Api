c346b7feb9206cf33644c8939481cd1b826b9fe4
prosystem-pro <prosystem490@gmail.com>
2026-05-27 11:39:48 -0600

    Se configuró para ver local host en celular

--- index.js
+++ index.js
@@ -4,12 +4,36 @@ process.env.TZ = 'UTC';
 
 require('./src/Relaciones/Relaciones');
 
+const os = require('os');
+
 const App = require('./src/app');
 
 const PORT = process.env.PORT || 3000;
 
+
+const networkInterfaces = os.networkInterfaces();
+let ipLocal = 'localhost';
+for (const interfaceName in networkInterfaces) {
+
+  const interfaces = networkInterfaces[interfaceName];
+
+  for (const iface of interfaces) {
+
+    if (
+      iface.family === 'IPv4' &&
+      !iface.internal
+    ) {
+      ipLocal = iface.address;
+      break;
+    }
+
+  }
+
+}
+
 App.listen(PORT, '0.0.0.0', () => {
-  console.log(`API disponible en: http://localhost:${PORT}/api`);
+  console.log(`http://localhost:${PORT}/api`);
+  console.log(`http://${ipLocal}:${PORT}/api`);
 });
 
 module.exports = App;
